0cd869eb0937c1199901333be442e0a5e6cc3653
Manny-03 <mannymishra2003@gmail.com>
2024-12-28 08:04:17 +0000

    Variables

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = "1234512";
+let accountEmail = "manas@google.com";
+var accountPassword = "12345";
+accountCity = "Bhubaneswar";
+let accountState;
+
+// accountId = "23456";     NOT ALLOWED
+
+accountEmail = "mm@google.com";
+accountPassword = "21212121";
+accountCity = "Bengaluru";
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
