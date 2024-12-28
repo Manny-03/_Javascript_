b4e502e603d04785d52dd728738ebaf17bec2216
Manny-03 <mannymishra2003@gmail.com>
2024-12-28 11:21:36 +0000

    Datatypes

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict";  //Treat all JS code as newer version
+
+// alert("Hi, I am MANAS");    WE ARE USING NODEJS, NOT BROWSER
+
+// Code readabilituy should be high
+
+let name = "Manas";
+let age = 21;
+let isLoggedin = true;
+
+/*
+    1. Number -> range = 2 ^ 53
+    2. bigInt
+    3. String -> ""
+    4. Boolean -> true/false
+    5. null -> Standalone value
+    6. Undefined -> Value not assigned
+    7. Symbol -> Used to define uniqueness
+    
+    8. Object
+
+*/
+
+
+//IMPORTANT
+console.log(typeof null);   //Type of null => OBJECT
+console.log(typeof undefined);   //Type of undefined => UNDEFINED
