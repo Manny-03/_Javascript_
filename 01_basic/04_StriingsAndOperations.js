776248e459430ba44f039b402376dd6eb11533d1
Manny-03 <mannymishra2003@gmail.com>
2024-12-29 18:30:47 +0000

    This includes tricky conversions and conversion patterns

---
+++ 01_basic/04_StriingsAndOperations.js
@@ -0,0 +1,17 @@
+let str1 = "Hello";
+let str2 = " Manas";
+let str3 = str1 + str2;
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+console.log(true);
+console.log(+true);
+console.log(+"");
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
