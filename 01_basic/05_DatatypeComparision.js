915f8937a4d874f72d610c5be6c6b4f14e8bc2f5
Manny-03 <mannymishra2003@gmail.com>
2024-12-29 19:28:18 +0000

    Comparision between different datatypes

---
+++ 01_basic/05_DatatypeComparision.js
@@ -0,0 +1,19 @@
+// console.log(1 < 2);
+// console.log(1 > 2);
+// console.log(1 == 2);
+// console.log(1 != 2);
+// These data types are easy to understand and compare
+
+console.log("1" > 2);
+console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);  /*COMPARISION CONVERT NULL TO A NUMBER, TREATING IT AS 0.
+                        THAT'S WHY "null > 0" IS FALSE & "null >= 0" IS TRUE. */
+
+console.log(undefined == 0);
+console.log(undefined >= 0);
+console.log(undefined < 0);
+
+console.log("2" === 2);  // === checks whether the value and the datatype are same or not
