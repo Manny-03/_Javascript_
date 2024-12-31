95bde0057bd38db1d66b84ffe4095119aa973640
Manny-03 <mannymishra2003@gmail.com>
2024-12-31 13:09:30 +0000

    Maths and Numbers Modified

--- 01_basic/08_NumbersAndMaths.js
+++ 01_basic/08_NumbersAndMaths.js
@@ -31,3 +31,13 @@ console.log(Math.floor(4.9));  //Always takes the absolute part of the number on
 
 console.log(Math.min(4,3,6,8));
 console.log(Math.max(4,3,6,8));
+
+console.log(Math.random());   // random values will be always between 0 and 1
+console.log((Math.random() * 10) + 1);
+console.log(Math.floor(Math.random() * 10) + 1);
+
+
+// If we want our number to be in between 10 and 20, then we need to use a formula , i.e ,
+let min = 10;
+let max = 20;
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
