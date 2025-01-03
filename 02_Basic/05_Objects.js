b24e0f1ad5a5c3f24084565fc021aaa9b0137ae2
Manny-03 <mannymishra2003@gmail.com>
2025-01-03 10:44:05 +0000

    Object Destructuring

---
+++ 02_Basic/05_Objects.js
@@ -0,0 +1,9 @@
+const course = {
+    courseName : "JS in hindi",
+    coursePrice : "999",
+    courseInstructor : "Hitesh Choudhary"
+}
+
+const {courseInstructor : instructor} = course;
+// console.log(courseInstructor);
+console.log(instructor);
