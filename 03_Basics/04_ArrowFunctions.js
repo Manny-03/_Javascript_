4ff8495cc33f20d779c6c050014332921957e8f6
Manny-03 <mannymishra2003@gmail.com>
2025-01-06 19:29:50 +0000

    More on this keyword

--- 03_Basics/04_ArrowFunctions.js
+++ 03_Basics/04_ArrowFunctions.js
@@ -3,8 +3,20 @@ const user = {
     price: 199,
     welcomeMessage : function(){
         console.log(`${this.username}, welcome to the website..`);
+        console.log(this);
     }
 }
 user.welcomeMessage();
 user.username = "Manny";
 user.welcomeMessage();
+
+console.log(this);
+
+//WINDOW is the global object in the browser.
+
+function chai(){
+    let username = "manas";
+    console.log(this);
+    console.log(this.username);   // "this" is not valid inside a function. it is valid only inside an object.
+}
+chai();
