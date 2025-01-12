4ba8e3a0badb315d54e6564828059a214659b304
Manny-03 <mannymishra2003@gmail.com>
2025-01-12 09:01:32 +0000

    Arrow function and this keyword

--- 03_Basics/04_ArrowFunctions.js
+++ 03_Basics/04_ArrowFunctions.js
@@ -20,3 +20,24 @@ function chai(){
     console.log(this.username);   // "this" is not valid inside a function. it is valid only inside an object.
 }
 chai();
+
+const func = () => {
+    let username = "Manas";
+    console.log(this.username);   
+}
+func();
+
+const addTwo = (num1,num2) => {
+    return num1 + num2;
+}
+console.log(addTwo(2,3));
+
+const addNum = (num1,num2) => num1 + num2;   //This type of declaring arrow function is known as "IMPLICIT RETURN".
+console.log(addNum(3,4));   
+
+/*
+    While declaring the arrow functions, if {} are used, then "return" must be written and if not used, then "return should not be used". 
+*/
+
+const retObj = () => ({username : "Manas"});
+console.log(retObj());
