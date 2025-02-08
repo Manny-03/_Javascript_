adadcc1c9b5445419fc8343081059b5d0cffeb98
Manny-03 <mannymishra2003@gmail.com>
2025-02-08 18:21:17 +0000

    While

---
+++ 05_Iterations/02_While.js
@@ -0,0 +1,12 @@
+let i = 0;
+while(i <= 10){
+    console.log(`The value is ${i}`);
+    i++;
+}
+
+const myArray = ["flash","superman","batman","green lantern"];
+let arrIndex = 0;
+while(arrIndex < myArray.length){
+    console.log(`The superhero at position ${arrIndex} is ${myArray[arrIndex]}`);
+    arrIndex++;
+}
