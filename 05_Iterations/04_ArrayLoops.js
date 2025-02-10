5127e1cc58ad373f6699b4ef89a8ceee5509f31d
Manny-03 <mannymishra2003@gmail.com>
2025-02-10 11:01:39 +0000

    Array Loops and High Order Loops

--- 05_Iterations/04_ArrayLoops.js
+++ 05_Iterations/04_ArrayLoops.js
@@ -24,3 +24,92 @@ console.log(map);
 for(const [key , value] of map){
     console.log(key,":-",value);
 }
+
+// const myObject = {   //FOR-OFF loop does not work on objects as objects are not iterable  
+//     'game1' : 'NFS',
+//     'game2' : 'Spiderman',
+//     'game3' : 'GTA V'
+// }
+// for(const [key,values] of myObject){
+//     console.log(key,":-",values);
+// }
+
+
+//FOR-IN LOOP
+
+const myObject = {
+    js: 'javascript',
+    cpp: 'c++',
+    rb: 'ruby',
+    swift: 'swift by apple'
+};
+for(const key in myObject){
+    // console.log(key);
+    // console.log(myObject[key]);
+    console.log(`${key} is used to represent ${myObject[key]}`);
+}
+
+const programingLanguages = ['js','cpp','rb','swift'];
+for(const keys in programingLanguages){
+    // console.log(keys);    //This will print the index number of the array
+    console.log(programingLanguages[keys]);
+}
+
+// const newMap = new Map();       //Maps are not iterabe using FOR-IN LOOPS
+// newMap.set('js',"JavaScript");
+// newMap.set('cpp',"C++");
+// newMap.set('rb',"Ruby");
+// newMap.set('swift',"Swift");
+// newMap.set('py',"Python"); 
+// for(const key in newMap){
+//     console.log(key);
+// }
+
+
+
+
+//FOR-EACH LOOP
+
+const codingLanguage = ["Javascript","Ruby","Python","Java",'C++'];
+codingLanguage.forEach( function (item) {
+    console.log(item);
+} );
+
+
+const coding = ["Javascript","Ruby","Python","Java","C++"];
+// coding.forEach( (item) => {
+//     console.log(item);
+// } )
+
+// function printMe(item){
+//     console.log(item);
+// }
+// coding.forEach(printMe);
+
+coding.forEach( (item,index,arr) => {
+    console.log(item,index,arr);
+})
+
+
+const myCodingLanguages = [
+    {
+        languageName : "Javascript",
+        languageFileName : "js"
+    },
+    {
+        languageName : "C++",
+        languageFileName : "cpp"
+    },
+    {
+        languageName : "Java",
+        languageFileName : "java"
+    },
+    {
+        languageName : "Python",
+        languageFileName : "py"
+    }
+]
+myCodingLanguages.forEach( (item)=>{
+    console.log(item.languageName);
+    console.log(item.languageFileName);
+} )
